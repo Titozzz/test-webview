52d5c2bdc5a530b48e052e262390e26a49f5c9b9
Thibault Malbranche <malbranche.thibault@gmail.com>
2022-06-14 12:04:42 +0200

    wip

---
+++ android/src/main/java/com/reactnativecommunity/webview/RNCBasicAuthCredential.java
@@ -0,0 +1,11 @@
+package com.reactnativecommunity.webview;
+
+class BasicAuthCredential {
+    String username;
+    String password;
+
+    BasicAuthCredential(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
--- android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
@@ -20,6 +21,10 @@ public class RNCWebViewManager extends SimpleViewManager<RNCWebView>
         implements RNCWebViewManagerInterface<RNCWebView> {
 
     private final ViewManagerDelegate<RNCWebView> mDelegate;
+    protected RNCWebViewConfig mWebViewConfig;
+    protected boolean mAllowsFullscreenVideo = false;
+    protected @Nullable String mDownloadingMessage = null;
+    protected @Nullable String mLackPermissionToDownloadMessage = null;
 
     public RNCWebViewManager(ReactApplicationContext context) {
         mDelegate = new RNCWebViewManagerDelegate<>(this);
@@ -41,12 +46,18 @@ public class RNCWebViewManager extends SimpleViewManager<RNCWebView>
     @NonNull
     @Override
     protected RNCWebView createViewInstance(@NonNull ThemedReactContext context) {
-        return RNCWebViewManagerImpl.createViewInstance(context);
+        return RNCWebViewManagerImpl.createViewInstance(context, mWebViewConfig, mDownloadingMessage, mLackPermissionToDownloadMessage, mAllowsFullscreenVideo);
     }
 
     @Override
     @ReactProp(name = "color")
     public void setColor(RNCWebView view, @Nullable String color) {
         RNCWebViewManagerImpl.setColor(view, color);
+    }
+
+    @Override
+    @ReactProp(name = "source")
+    public void setSource(RNCWebView view, @Nullable ReadableMap value) {
+        RNCWebViewManagerImpl.setSource(view, value);
     }
 }
